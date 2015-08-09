d7bfdd1cd322a319d17957fcd0c66ef5a287b2cc
diamant1991 <diamant1988@mail.ru>
2015-08-09 13:01:07 +0300

    pravki

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ $('input,textarea').focus(function(){
 $('input,textarea').blur(function(){
   $(this).attr('placeholder',$(this).data('placeholder'));
 });
+
+document.documentElement.id = "js";
+
+$(document).ready(function() {
+ 
+    $('.input-text').each(function(){
+        if($(this).val() != '') $(this).prev().addClass('hide');
+    });
+ 
+    $('.input-text').blur(function() {
+        if ($(this).val() == '') $(this).prev().removeClass('hide');
+    });
+ 
+    $('.input-text').focus(function() {
+        $(this).prev().addClass('hide');
+    });
+ 
+    $('.input-text').mouseover(function() {
+        if ($(this).val() != '') $(this).prev().addClass('hide');
+    });
+});
